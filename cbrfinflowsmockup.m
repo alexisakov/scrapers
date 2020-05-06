029215d5e99c7b064290bf851b71d2912697d985
alex isakov <ale.isakov@gmail.com>
2020-05-06 19:28:16 +0300

    Update cbrfinflowsmockup.m

--- cbrfinflowsmockup.m
+++ cbrfinflowsmockup.m
@@ -1,5 +1,7 @@
 path="http://www.cbr.ru/Collection/Collection/File/27842/finflows_20200427.pdf";
-ch=Import[path,{"Pages",4}];
-jc=Cases[ch,_JoinedCurve,Infinity];
-ln = jc[[5,2,1,;;,2]];
-ListPlot[ln,Joined->True,Filling->Axis,FillingStyle->HatchFilling[],PlotStyle->Red]
+
+h = Import[path, "Pages"];
+jc = Cases[h, Style[{___, _JoinedCurve, ___}, ___, RGBColor[0.933`, 0.06670000000000001`, 0.2`, 1.`], ___], Infinity]
+noleg = jc[[;; ;; 2]];
+ln = noleg[[;; , 1, 1, 2, 1, ;; , 2]];
+Export["cbrflow.xlsx", ln]
